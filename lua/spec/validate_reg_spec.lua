c5170568861223634602c7b6e0f1ac9718c72a19
Gennaro Tedesco <gennarotedesco@gmail.com>
2021-03-01 18:53:46 +0100

    added function to validate register string

---
+++ lua/spec/validate_reg_spec.lua
@@ -0,0 +1,14 @@
+describe("validate custom register string", function()
+   local config = require("nvim-peekup.config")
+   local valid_reg, invalid_reg
+
+   setup(function()
+	  valid_reg = 'a'
+	  invalid_reg = '!'
+   end)
+
+   it("is_comment_block_surround #surround", function()
+	  assert.are_same(config.validate_reg(valid_reg), valid_reg)
+	  assert.are_same(config.validate_reg(invalid_reg), '*')
+   end)
+end)
