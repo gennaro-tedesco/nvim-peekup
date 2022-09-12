f2bb3e8378ece77caf3ef0f9aab25b382faac94d
Gennaro Tedesco <gennaro@billie.io>
2022-09-12 23:39:20 +0200

    added pre-commit hooks to format lua files

--- lua/nvim-peekup/config.lua
+++ lua/nvim-peekup/config.lua
@@ -1,33 +1,40 @@
 local reg_chars = {}
-local chars = 'abcdefghijklmnopqrstuvwxyz0123456789-.%'
+local chars = "abcdefghijklmnopqrstuvwxyz0123456789-.%"
 if vim.fn.has("clipboard") == 1 then
-   chars = 'abcdefghijklmnopqrstuvwxyz0123456789*+-.%'
+	chars = "abcdefghijklmnopqrstuvwxyz0123456789*+-.%"
 end
-chars:gsub(".",function(c) table.insert(reg_chars,c) end)
+chars:gsub(".", function(c)
+	table.insert(reg_chars, c)
+end)
 
 local geometry = {
-   width = 0.7,
-   height = 0.7,
-   title = 'Registers',
-   name = 'peekup',
-   wrap = true,
+	width = 0.7,
+	height = 0.7,
+	title = "Registers",
+	name = "peekup",
+	wrap = true,
 }
 
 local function validate_reg(s)
-   for _,v in ipairs(reg_chars) do if v == s then return s end end return '*'
+	for _, v in ipairs(reg_chars) do
+		if v == s then
+			return s
+		end
+	end
+	return "*"
 end
 
-local paste_reg = '*'
+local paste_reg = "*"
 local on_keystroke = {
-   delay = '700ms',
-   autoclose = true,
-   padding = 3,
-   paste_reg = validate_reg(paste_reg)
+	delay = "700ms",
+	autoclose = true,
+	padding = 3,
+	paste_reg = validate_reg(paste_reg),
 }
 
 return {
-   reg_chars = reg_chars,
-   geometry = geometry,
-   on_keystroke = on_keystroke,
-   validate_reg = validate_reg,
+	reg_chars = reg_chars,
+	geometry = geometry,
+	on_keystroke = on_keystroke,
+	validate_reg = validate_reg,
 }
--- lua/nvim-peekup/init.lua
+++ lua/nvim-peekup/init.lua
@@ -1,23 +1,21 @@
 --[[ this module exposes the interface to open the peekup
 window containing the registers content. Moreover it
-sets the corresponding buffer options and commands on keystroke ]] local peekup =
-    require("nvim-peekup.peekup")
+sets the corresponding buffer options and commands on keystroke ]]
+local peekup = require("nvim-peekup.peekup")
 local config = require("nvim-peekup.config")
 
 local function set_peekup_opts(buf, paste_where)
-    vim.api.nvim_buf_set_option(buf, 'filetype', 'peek')
-    vim.api.nvim_buf_set_option(buf, 'bufhidden', 'wipe')
-    vim.api.nvim_buf_set_option(buf, 'modifiable', false)
-    vim.api.nvim_buf_set_option(buf, 'readonly', true)
-    vim.api.nvim_buf_set_keymap(buf, 'n', '<ESC>', ':q<CR>',
-                                {nowait = true, noremap = true, silent = true})
-    vim.api.nvim_buf_set_keymap(buf, 'n', '<C-j>', '<C-e>',
-                                {nowait = true, noremap = true, silent = true})
-    vim.api.nvim_buf_set_keymap(buf, 'n', '<C-k>', '<C-y>',
-                                {nowait = true, noremap = true, silent = true})
+	vim.api.nvim_buf_set_option(buf, "filetype", "peek")
+	vim.api.nvim_buf_set_option(buf, "bufhidden", "wipe")
+	vim.api.nvim_buf_set_option(buf, "modifiable", false)
+	vim.api.nvim_buf_set_option(buf, "readonly", true)
+	vim.api.nvim_buf_set_keymap(buf, "n", "<ESC>", ":q<CR>", { nowait = true, noremap = true, silent = true })
+	vim.api.nvim_buf_set_keymap(buf, "n", "<C-j>", "<C-e>", { nowait = true, noremap = true, silent = true })
+	vim.api.nvim_buf_set_keymap(buf, "n", "<C-k>", "<C-y>", { nowait = true, noremap = true, silent = true })
 
-    -- setting markers
-    vim.api.nvim_exec([[
+	-- setting markers
+	vim.api.nvim_exec(
+		[[
    function! SetMarks() abort
 	  execute 'keeppatterns /^Numerical'
 	  execute 'mark n'
@@ -29,39 +27,45 @@ local function set_peekup_opts(buf, paste_where)
    endfunction
 
    call SetMarks()
-   ]], false)
+   ]],
+		false
+	)
 
-    -- setting peekup keymaps
-    for _, v in ipairs(config.reg_chars) do
-        vim.api.nvim_buf_set_keymap(buf, 'n', v,
-                                    ':lua require"nvim-peekup.peekup".on_keystroke(\"' ..
-                                        v .. '\",\"' .. paste_where .. '\")<cr>',
-                                    {
-            nowait = true,
-            noremap = true,
-            silent = true
-        })
-    end
-    vim.api.nvim_buf_set_keymap(buf, 'n', '<Down>', ']`',
-                                {nowait = true, noremap = true, silent = true})
-    vim.api.nvim_buf_set_keymap(buf, 'n', '<Up>', '[`',
-                                {nowait = true, noremap = true, silent = true})
+	-- setting peekup keymaps
+	for _, v in ipairs(config.reg_chars) do
+		vim.api.nvim_buf_set_keymap(
+			buf,
+			"n",
+			v,
+			':lua require"nvim-peekup.peekup".on_keystroke("' .. v .. '","' .. paste_where .. '")<cr>',
+			{
+				nowait = true,
+				noremap = true,
+				silent = true,
+			}
+		)
+	end
+	vim.api.nvim_buf_set_keymap(buf, "n", "<Down>", "]`", { nowait = true, noremap = true, silent = true })
+	vim.api.nvim_buf_set_keymap(buf, "n", "<Up>", "[`", { nowait = true, noremap = true, silent = true })
 end
 
 local function peekup_open(paste_where)
-    -- avoid opening nested peekup
-    if vim.bo.filetype == "peek" then return nil end
+	-- avoid opening nested peekup
+	if vim.bo.filetype == "peek" then
+		return nil
+	end
 
-    local lines = peekup.reg2t(paste_where)
-    local peekup_buf = peekup.floating_window(config.geometry)
-    table.insert(lines, 1, peekup.centre_string(config.geometry.title))
-    table.insert(lines, 2, '')
+	local lines = peekup.reg2t(paste_where)
+	local peekup_buf = peekup.floating_window(config.geometry)
+	table.insert(lines, 1, peekup.centre_string(config.geometry.title))
+	table.insert(lines, 2, "")
 
-    if paste_where == nil then paste_where = '' end
+	if paste_where == nil then
+		paste_where = ""
+	end
 
-    vim.api.nvim_buf_set_lines(peekup_buf, 0, -1, true, lines)
-    set_peekup_opts(peekup_buf, paste_where)
+	vim.api.nvim_buf_set_lines(peekup_buf, 0, -1, true, lines)
+	set_peekup_opts(peekup_buf, paste_where)
 end
 
-return {peekup_open = peekup_open}
-
+return { peekup_open = peekup_open }
