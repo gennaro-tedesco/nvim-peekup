2a14f4ae8b163f69a80efa2a56755e87505074f8
Gennaro Tedesco <gennaro@billie.io>
2022-09-12 23:21:27 +0200

    avoid opening nested peekup by checking filetype

--- lua/spec/validate_reg_spec.lua
+++ lua/spec/validate_reg_spec.lua
@@ -1,14 +1,14 @@
 describe("validate custom register string", function()
-   local config = require("nvim-peekup.config")
-   local valid_reg, invalid_reg
+    local config = require("nvim-peekup.config")
+    local valid_reg, invalid_reg
 
-   setup(function()
-	  valid_reg = 'a'
-	  invalid_reg = '!'
-   end)
+    setup(function()
+        valid_reg = 'a'
+        invalid_reg = '!'
+    end)
 
-   it("validate register #validate_reg", function()
-	  assert.are_same(config.validate_reg(valid_reg), valid_reg)
-	  assert.are_same(config.validate_reg(invalid_reg), '*')
-   end)
+    it("validate register #validate_reg", function()
+        assert.are_same(config.validate_reg(valid_reg), valid_reg)
+        assert.are_same(config.validate_reg(invalid_reg), '*')
+    end)
 end)
